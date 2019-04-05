e0cba186d60de959a2e0bf6e61d839ca8f13b5a1
sunghyun.keum <sunghyun.keum@woowahan.com>
2019-04-06 04:34:12 +0900

    문자열 계산기 구현 완료

--- string-calculator/src/main/java/StringCalculator.java
+++ string-calculator/src/main/java/StringCalculator.java
@@ -2,10 +2,13 @@ public class StringCalculator {
     public static int splitAndSum(String text) {
         int result = 0;
 
-        if(text == null || text.isEmpty()) {
+        if (text == null || text.isEmpty()) {
             return 0;
-        }else{
-            result =  Integer.parseInt(text);
+        } else {
+            String[] values = text.split(",|:");
+            for (String value : values) {
+                result +=Integer.parseInt(value);
+            }
         }
         return result;
     }
