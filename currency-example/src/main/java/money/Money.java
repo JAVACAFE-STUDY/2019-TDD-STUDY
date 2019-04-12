44dcc7003376cd398d028551dfef7557c8f74e44
sunghyun.keum <sunghyun.keum@woowahan.com>
2019-04-13 03:31:53 +0900

    - 큰 테스트를 작은 테스트로 줄여서 발전을 나타낼 수 있도록 했다. - 우리에게 필요한 계산에 대한 가능한 메타포들을 신중히 생각해봤다. - 새 메타포에 기반하여 기존의 테스트를 재작성했다 - 테스트를 빠르게 컴파일했다. - 그리고 테스트를 실행했다. - 진짜 구현을 만들기 위해 필요한 리팩토링을 약간의 전율과 함께 기대했다.

--- currency-example/src/main/java/money/Money.java
+++ currency-example/src/main/java/money/Money.java
@@ -1,6 +1,6 @@
 package money;
 
-public class Money {
+public class Money implements Expression{
 
     protected int amount;
     protected String currency;
@@ -37,4 +37,7 @@ public class Money {
         return new Money(amount, "CHF");
     }
 
+    public Money plus(Money addend) {
+        return new Money(amount + addend.amount, currency);
+    }
 }
