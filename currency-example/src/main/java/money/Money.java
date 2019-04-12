92d7325bf223750e7796a0963fffa223750f9667
sunghyun.keum <sunghyun.keum@woowahan.com>
2019-04-12 23:23:31 +0900

    - 동일한 메서드(times)의 두 변이형 메서드 서명부를 통일시킴으로써 중복 제거를 향해 한 단계 더 전진했다. - 최소한 메서드 선언부만이라도 공통 상위 클래스로(superclass)로 옮겼다. - 팩토리 메서드를 도입하면 테스트 코드에서 콘크리트 하위 클래스의 존재 사실을 분리해냈다. - 하위 클래스가 사라지면 몇몇 테스트는 불필요한 여분의 것이 된다는 것을 인식했다. 하지만 일단 그냥 뒀다.

--- currency-example/src/main/java/money/Money.java
+++ currency-example/src/main/java/money/Money.java
@@ -1,7 +1,17 @@
 package money;
 
-public class Money {
+public abstract class Money {
+    abstract Money times(int multiplier);
+
     protected int amount;
+
+    public static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
--- currency-example/src/test/java/money/MoneyTest.java
+++ currency-example/src/test/java/money/MoneyTest.java
@@ -28,25 +28,24 @@ public class MoneyTest {
 
     @Test
     public void testMultiplication() {
-        Dollar five= new Dollar(5);
-        assertEquals(new Dollar(15), five.times(3));
-        assertEquals(new Dollar(10), five.times(2));
-    }
-
-    @Test
-    public void testEquality() {
-        assertTrue(new Dollar(5).equals(new Dollar(5)));
-        assertFalse(new Dollar(5).equals(new Dollar(6)));
-        assertTrue(new Franc(5).equals(new Franc(5)));
-        assertFalse(new Franc(5).equals(new Franc(6)));
-        assertFalse(new Franc(5).equals(new Dollar(5)));
+        Money money= Money.dollar(5);
+        assertEquals(Money.dollar(15), money.times(3));
+        assertEquals(Money.dollar(10), money.times(2));
     }
 
     @Test
     public void testFrancMultiplication() {
-        Franc five= new Franc(5);
-        assertEquals(new Franc(15), five.times(3));
-        assertEquals(new Franc(10), five.times(2));
+        Money money= Money.franc(5);
+        assertEquals(Money.franc(15), money.times(3));
+        assertEquals(Money.franc(10), money.times(2));
     }
 
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
 }
